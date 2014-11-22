9843dfbe9d16780318cc725fa89b8f9328bae107
Dan Fox <iamdanfox@gmail.com>
2014-11-22 15:15:06 +0000

    Combined config files, new test command

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
 var webpack = require('webpack');
 
-module.exports = {
-  entry: {
-    app: './main.cjsx',
-    vendor: ['react', 'reflux', 'react-spinkit', 'timeago', 'fetch', 'github-api', 'es6-promise']
-  },
-  output: {
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['', '.js', '.cjsx', '.less']
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
-  ],
-  module: {
-    loaders: [
-      { test: /\.coffee$/, loaders: ['coffee-loader']},
-      { test: /\.cjsx$/, loaders: ['coffee-loader', 'cjsx-loader']},
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
-      { test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader'] }
-    ]
+module.exports = [
+  {
+    entry: {
+      app: './main.cjsx',
+      vendor: ['react', 'reflux', 'react-spinkit', 'timeago', 'fetch', 'github-api', 'es6-promise']
+    },
+    output: {
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['', '.js', '.cjsx', '.less']
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+    ],
+    module: {
+      loaders: [
+        { test: /\.coffee$/, loaders: ['coffee-loader']},
+        { test: /\.cjsx$/, loaders: ['coffee-loader', 'cjsx-loader']},
+        { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+        { test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader'] }
+      ]
+    }
+  }, {
+    entry: './test/Test.coffee',
+    output: {
+      filename: 'test.bundle.js',
+      target: 'node'
+    },
+    resolve: {
+      extensions: ['', '.js', '.cjsx', '.less']
+    },
+    module: {
+      loaders: [
+        { test: /\.coffee$/, loaders: ['coffee-loader']},
+        { test: /\.cjsx$/, loaders: ['coffee-loader', 'cjsx-loader']},
+        { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+        { test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader'] }
+      ]
+    }
   }
-};
+]
